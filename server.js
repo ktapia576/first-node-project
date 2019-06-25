c57f5ba6093ad06e87418cd38b2844ae9f12fc45
ktapia576 <ktapia576@hotmail.com>
2019-06-25 15:40:30 -0400

    Format Code for easier reading

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime() + "<br>");
   res.write(req.url);
-  var q = url.parse(req.url, true).query;
+  var parsedURL = url.parse(req.url, true);
+  var q = parsedURL.query;
   var txt = q.year + " " + q.month + " " + q.day;
   res.write("<br>"+txt);
   res.end();
