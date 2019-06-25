e185338f897408dd67dd509089c5013c6ff21f54
ktapia576 <ktapia576@hotmail.com>
2019-06-25 15:10:19 -0400

    Add Url module and use parse url query

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
+var url = require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime() + "\n<br>");
+  res.write("The date and time are currently: " + dt.myDateTime() + "<br>");
   res.write(req.url);
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.write("<br>"+txt);
   res.end();
 }).listen(8766);
