aa15bce5c4927ff6cec9f5e3aea75ef89157415e
ktapia576 <ktapia576@hotmail.com>
2019-06-25 15:44:37 -0400

    Add comments

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime() + "<br>");
   res.write(req.url);
-  var parsedURL = url.parse(req.url, true);
-  var q = parsedURL.query;
+  var parsedURL = url.parse(req.url, true); // returns a URL object
+  var q = parsedURL.query;  // returns an object: { year: 2017, month: 'february', day: 'Monday' }
   var txt = q.year + " " + q.month + " " + q.day;
   res.write("<br>"+txt);
   res.end();
