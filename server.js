e10c1c2799e8c5fe27fd133e76bdfd608cfe4e35
ktapia576 <ktapia576@hotmail.com>
2019-06-26 13:36:26 -0400

    Format Code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 var url = require('url');
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime() + "<br>");
   res.write(req.url);
