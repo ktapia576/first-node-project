ad41e34fdc77e428a32e3916b886440d4db4819c
ktapia576 <ktapia576@hotmail.com>
2019-06-26 14:56:18 -0400

    Format Code better

--- fileserver.js
+++ fileserver.js
@@ -3,16 +3,16 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer((req, res) => {
-    var q = url.parse(req.url, true);   // Get URL object
-    var filename = "." + q.pathname;    // Get filename from path in URL
+  var q = url.parse(req.url, true);   // Get URL object
+  var filename = "." + q.pathname;    // Get filename from path in URL
 
-    fs.readFile(filename, (err, data) => {
-        if (err) {
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          return res.end("404 Not Found");
-        }  
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-      });
+  fs.readFile(filename, (err, data) => {
+    if (err) { 
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8766);
