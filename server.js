ed502f47adeff61129ed49965d90c489911e315f
ktapia576 <ktapia576@hotmail.com>
2019-06-25 22:22:13 -0400

    Format Code

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime() + "<br>");
   res.write(req.url);
+
   var parsedURL = url.parse(req.url, true); // returns a URL object
   var q = parsedURL.query;  // returns an object: { year: 2017, month: 'february', day: 'Monday' }
   var txt = q.year + " " + q.month + " " + q.day;
+  
   res.write("<br>"+txt);
   res.end();
 }).listen(8766);
