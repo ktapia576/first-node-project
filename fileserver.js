978f515adb35f17127378f3e78e90c4e7e8b8fe2
ktapia576 <ktapia576@hotmail.com>
2019-07-01 14:28:17 -0400

    Added index.html with URLrouting

--- fileserver.js
+++ fileserver.js
@@ -6,6 +6,10 @@ http.createServer((req, res) => {
   var q = url.parse(req.url, true);   // Get URL object
   var filename = "." + q.pathname;    // Get filename from path in URL
 
+  if(filename === './'){  // If no path specified in URL, send to index.html
+    filename = './index.html';
+  }
+
   fs.readFile(filename, (err, data) => {
     if (err) { 
       res.writeHead(404, {'Content-Type': 'text/html'});
