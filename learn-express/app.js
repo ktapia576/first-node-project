d9bdbaa725763e297995652c5043d22b114b1791
ktapia576 <ktapia576@hotmail.com>
2019-07-21 14:59:09 -0400

    Add Create and Update Members

--- learn-express/app.js
+++ learn-express/app.js
@@ -4,7 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 // Initializes Middleware for logging
 //app.use(logger);
+
+app.use('/api/members', require('./routes/api/members'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
--- learn-express/routes/api/members.js
+++ learn-express/routes/api/members.js
@@ -1,16 +1,54 @@
 const express = require('express');
 const router = express.Router();
-const members = require('./Members');
+const members = require('../../Members');
+const uuid = require('uuid');
 
 // This route gets all Members
-router.get('/api/members', (req, res) => res.json(members));
+router.get('/', (req, res) => res.json(members));
 
 // This gets a single member
-router.get('/api/members/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const found = members.some(member => member.id === parseInt(req.params.id));
 
   if (found){
     res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+  }  
+});
+
+// Create Member
+router.post('/', (req, res) => {
+  const newMember = {
+    // Have database create random id 
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  }
+
+  if(!newMember.name || !newMember.email){
+    return res.status(400).json({ msg: 'Please include a name and email'}); // Have return so you don't need an else. You would get an error "Headers already sent" if not
+  } 
+
+  members.push(newMember);
+  res.json(members);
+});
+
+// Update Member
+router.put('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found){
+    const updatedMember = req.body;
+    members.forEach(member => {
+      if(member.id === parseInt(req.params.id)){
+        member.name = updatedMember.name ? updatedMember.name : member.name;  // Check if name was sent with req, if not keep old name
+        member.email = updatedMember.email ? updatedMember.email : member.email;
+
+        res.json({ msg: 'Member updated', member});
+      }
+    });
   } else {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
   }  
